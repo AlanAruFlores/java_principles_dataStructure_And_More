7b68beaa9142d0e6e37f5ab91831ec52707eed9c
AlanAruFlores <alanaruquipa242@gmail.com>
2025-02-10 23:04:20 -0300

    proxy pattern

---
+++ Patrones_Estructurales/src/com/ar/proxy/Main.java
@@ -0,0 +1,12 @@
+package com.ar.proxy;
+
+/**
+ * Proxy:
+ * 
+ * Proxy es un patrón de diseño estructural que te permite proporcionar un sustituto o marcador
+ *  de posición para otro objeto. Un proxy controla el acceso al objeto original,
+ *  permitiéndote hacer algo antes o después de que la solicitud llegue al objeto original.
+ */
+public class Main {
+
+}
