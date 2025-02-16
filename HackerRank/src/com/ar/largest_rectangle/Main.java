a4c01a8f3f545673ba0b783eb168824745da5d20
AlanAruFlores <alanaruquipa242@gmail.com>
2025-02-15 21:33:44 -0300

    largest rectangle hackerrank

---
+++ HackerRank/src/com/ar/largest_rectangle/Main.java
@@ -0,0 +1,48 @@
+package com.ar.largest_rectangle;
+import java.util.*;
+
+public class Main {
+
+    /*
+     * Complete the 'largestRectangle' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY h as parameter.
+     */
+
+    public static long largestRectangle(List<Integer> h) {
+    // Write your code here
+        Stack<Integer> valuesTemp = new Stack<>();
+        
+        long largestRectangleResult = 0l;
+        
+        for(int i = 0 ; i < h.size(); i++){
+            
+            //Evaluamos en adelante
+            cicloCreciente: for(int j = i ; j < h.size(); j++){    
+                if(h.get(j) >= h.get(i)){
+                    valuesTemp.push(h.get(i));
+                }
+                else{
+                    break cicloCreciente;
+                }
+                
+            }
+            
+            //Evaluamos desde atras
+            cicloDecreciente: for(int j = i-1; j>= 0; j--){
+                if(h.get(j) >= h.get(i)){
+                    valuesTemp.push(h.get(i));
+                }
+                else{
+                    break cicloDecreciente;
+                }
+            }
+            
+            largestRectangleResult = (long) Math.max(largestRectangleResult,getStackSum(valuesTemp));
+            
+            valuesTemp.clear();
+        }    
+        return largestRectangleResult;
+    }
+}
